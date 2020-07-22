c6e8c943e7d5e2df7c6bba7e10d333b11017c562
Praveen Kumar <praveenbapuram.explore@gmail.com>
2020-07-22 18:54:03 +0530

    Adding bottom Navigation bar.

--- flutter_meals_app/lib/screens/tabs_screen.dart
+++ flutter_meals_app/lib/screens/tabs_screen.dart
@@ -7,31 +7,47 @@ class TabsScreen extends StatefulWidget {
   _TabsScreenState createState() => _TabsScreenState();
 }
 
+List<Map<String, Object>> _pages = [
+  {'page': CategoriesScreen(), 'title': 'Categories'},
+  {'page': FavouritesScreen(), 'title': 'Your Favourites'},
+];
+
 class _TabsScreenState extends State<TabsScreen> {
+  int selectPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      selectPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      //initialIndex: 1,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(tabs: <Widget>[
-            Tab(
-              icon: Icon(Icons.category),
-              text: 'Categories',
-            ),
-            Tab(
-              icon: Icon(Icons.star),
-              text: 'Favourites',
-            )
-          ]),
-        ),
-        body: TabBarView(children: <Widget>[
-          CategoriesScreen(),
-          FavouritesScreen(),
-        ]),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[selectPageIndex]['title']),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: selectPageIndex,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            title: Text('Categories'),
+            backgroundColor: Theme.of(context).primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            title: Text('Favourites'),
+            backgroundColor: Theme.of(context).primaryColor,
+          )
+        ],
+      ),
+      body: _pages[selectPageIndex]['page'],
     );
   }
 }
