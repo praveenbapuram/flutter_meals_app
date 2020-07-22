3931eb0dfbf684cc3abe5f63d41a18e74cb7ccb3
Praveen Kumar <praveenbapuram.explore@gmail.com>
2020-07-22 19:56:49 +0530

    creating a Drawer() for hamberger menu.

--- flutter_meals_app/lib/screens/tabs_screen.dart
+++ flutter_meals_app/lib/screens/tabs_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_meals_app/screens/categories_screen.dart';
 import 'package:flutter_meals_app/screens/favourites_screen.dart';
+import 'package:flutter_meals_app/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -24,6 +25,7 @@ class _TabsScreenState extends State<TabsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: MainDrawer(),
       appBar: AppBar(
         title: Text(_pages[selectPageIndex]['title']),
       ),
